e60f0fefd5110e7feb38f836e72c06fa6298ab18
cnikolai <cynthianikolai@gmail.comgit config --global user.email cynthianikolai@gmail.com>
2021-04-27 18:45:01 -0400

    checkpoint 1

--- Task-CoreData/Task-CoreData/Controllers/TaskController.swift
+++ Task-CoreData/Task-CoreData/Controllers/TaskController.swift
@@ -13,22 +13,36 @@ class TaskController {
     
     var tasks:[Task] = []
     
+    private lazy var fetchRequest: NSFetchRequest<Task> = {
+       let request = NSFetchRequest<Task>(entityName: "Task")
+        request.predicate = NSPredicate(value: true)
+        return request
+    }()
+    
+    private init() {}
+    
     func createTaskWith(name: String, notes: String?, dueDate: Date?) {
         let newTask = Task.init(name: name, notes: notes, dueDate: dueDate)
-        
+        tasks.append(newTask)
         CoreDataStack.saveContext()
     }
     
     func fetchTasks() {
+        let tasks = (try? CoreDataStack.context.fetch(self.fetchRequest)) ?? []
         
+        self.tasks = tasks
     }
     
     func update(task: Task, name: String, notes: String?, dueDate: Date?) {
+        task.name = name
+        task.notes = notes
+        task.dueDate = dueDate
         
+        CoreDataStack.saveContext()
     }
     
     func toggleIsComplete(task: Task) {
-        
+        task.isComplete.toggle()
+        CoreDataStack.saveContext()
     }
-
 }
--- Task-CoreData/Task-CoreData/Controllers/TaskDetailViewController.swift
+++ Task-CoreData/Task-CoreData/Controllers/TaskDetailViewController.swift
@@ -13,26 +13,39 @@ class TaskDetailViewController: UIViewController {
     @IBOutlet weak var taskNotesTextView: UITextView!
     @IBOutlet weak var taskDueDatePicker: UIDatePicker!
         
+    var task: Task?
+    var date: Date?
+    
     // MARK:- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateViews()
     }
     
-    @IBAction func saveButtonTapped(_ sender: UIBarButton) {
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let name = taskNameTextField.text, !name.isEmpty,
+              let notes = taskNotesTextView.text, !notes.isEmpty,
+              let dueDate = taskDueDatePicker?.date else { return }
+        if let task = task {
+            task.name = name
+            task.notes = notes
+            task.dueDate = dueDate
+            TaskController.sharedInstance.update(task: task, name: name, notes: notes, dueDate: dueDate)
+        } else {
+            TaskController.sharedInstance.createTaskWith(name: name, notes: notes, dueDate: dueDate)
+        }
+        navigationController?.popViewController(animated: true)
     }
     
     @IBAction func dueDatePickerDateChanged(_ sender: UIDatePicker) {
+        self.date = taskDueDatePicker?.date ?? Date() 
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        guard let task = task else { return }
+        taskNameTextField.text = task.name
+        taskNotesTextView.text = task.notes
+        taskDueDatePicker.date = task.dueDate ?? Date()
     }
-    */
-
 }
